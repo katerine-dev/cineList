646c25727e1ca0bba5471f4ae18c07da87214154
katewitkoski <ka.lwitkoski@gmail.com>
2024-10-31 15:46:31 -0300

    Recebe as configurações do sring initializr

---
+++ src/main/java/com/duckbill/cine_list/CineListApplication.java
@@ -0,0 +1,13 @@
+package com.duckbill.cine_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duckbill/cine_list/CineListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duckbill.cine_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
